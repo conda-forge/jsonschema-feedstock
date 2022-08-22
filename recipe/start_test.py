ee1c280001302025c954133651f72c43c2497aaa
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-08-21 21:57:54 -0500

    manually pick all test_*.py files

--- recipe/start_test.py
+++ recipe/start_test.py
@@ -3,20 +3,20 @@ import os
 import sys
 
 import pytest
+import jsonschema
 
 HERE = Path(__file__).parent
+TESTS = Path(jsonschema.__file__).parent / "tests"
 
 os.environ.update(JSON_SCHEMA_TEST_SUITE=str(HERE / "json"))
 
 PYTEST_ARGS = [
     "-vv",
-    "--pyargs",
-    "jsonschema",
     "--cov=jsonschema",
     "--cov-report=term-missing:skip-covered",
     "--no-cov-on-fail",
-    "--ignore-glob=*/_helpers.py",
     *sys.argv[1:],
+    *TESTS.rglob("test_*.py")
 ]
 
 print("PYTEST_ARGS", *PYTEST_ARGS)
